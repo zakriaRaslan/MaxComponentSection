58250911dd364c32a8b9da98e8c63eb74b1299b0
Z.Raslan <zikaraslan23@gmail.com>
2025-02-17 15:26:24 +0200

    using encapsulation to effect styles to element in projection

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'button[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
+  encapsulation:ViewEncapsulation.None
 })
 export class ButtonComponent {
 
--- src/app/shared/controll/controll.component.ts
+++ src/app/shared/controll/controll.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-controll',
   standalone: true,
   imports: [],
   templateUrl: './controll.component.html',
-  styleUrl: './controll.component.css'
+  styleUrl: './controll.component.css',
+  encapsulation:ViewEncapsulation.None
 })
 export class ControllComponent {
 label = input.required<string>();
