60cccb586c9ae1cb0c86a56ba8a319f085982815
Z.Raslan <zikaraslan23@gmail.com>
2025-02-21 23:01:38 +0200

    using Template Variables To get data from the newTicket form

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
 
+
+  onSubmit(title:string , textarea:string){
+
+    console.log(title + "*****" + textarea);
+  }
 }
